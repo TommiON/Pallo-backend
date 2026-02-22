01fc4178115ebd6cb086578971020df06a4b8ff3
TommiON <tommi.niittymies@gmail.com>
2026-02-22 19:34:51 +0200

    Player model started; util+test for randomly picking elements

---
+++ domainModel/player/Player.ts
@@ -0,0 +1,16 @@
+import Footedness from "./Footedness";
+
+export default class Player {
+    name: string;
+    age: number;
+
+    footedness: Footedness;
+
+    stamina: number;
+    pace: number;
+    strength: number;
+
+    constructor() {
+
+    }
+}
