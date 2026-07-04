21144d19e22615335a6527d84ca6a9717774726e
TommiON <tommi.niittymies@gmail.com>
2026-07-04 15:38:33 +0300

    Match-resolvauksen ensi askeleet, tallennetaan dummy-lopputulos

--- controllers/resolveMatches.ts
+++ controllers/resolveMatches.ts
@@ -1,22 +1,33 @@
 import { resolveMatch } from "../domainEngine/matches/MatchResolver";
-import { findMatchesBySeasonAndWeek, saveMatchesInBatch } from "../dataAccess/matchService";
+import { findMatchesBySeasonAndWeek, saveMatch, saveMatchesInBatch } from "../dataAccess/matchService";
+import { saveMatchEventsInBatch } from "../dataAccess/matchEventService";
 
 export const resolveMatches = async (season: number, week: number) => {
-    // 1. haetaan matchit tietyltä kierrokselta (tälle service dataAccessiin). Tukeeko tietomalli sitä että matsi voidaan pelata täysin eristetysti,
-    //      ei tarvitse tietää mitään esim. liigoista ja muusta ulkopuolisesta?
+    // Haetaan viikon ottelut ratkaistaviksi
     const matches = await findMatchesBySeasonAndWeek(season, week);
 
-    console.log('PELIPÄIVÄ! Kausi ', season, 'viikko', week)
-
-    matches.forEach((match) => {
-        console.log(match)
-    })
-
     // 2. palastellaan batcheiksi täällä?
+
     // 3. lähetetään DomainEnginen MatchResolveriin, joka palauttaa MatchEvent[]?
     // 4. assosioidaan MatchEventit Matcheihin
     // 4. saadaan vastaukseksi resolvoituja Matcheja, jotka tallennetaan
     //      tallennus pitää hoitaa transaktionaalisesti varmaankin
+
+    matches.forEach((match) => {
+        match.started = true;
+        match.events = resolveMatch();
+        match.events.forEach((event) => {
+            event.match = match;
+        });
+        saveMatchEventsInBatch(match.events);
+        match.finished = true;
+        saveMatch(match);
+
+        const tulos = match.getResult();
+        console.log(`Ottelu ${match.id}, lopputulos: ${tulos.homeGoals} - ${tulos.awayGoals}`);
+    })
+
+    
     // Ei paluudataa, heittää Erroreita jos tarpeen?
 
 
