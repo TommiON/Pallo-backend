292371ac222b9bc02f7e538980cf76aa55e23ad6
TommiON <tommi.niittymies@gmail.com>
2026-09-12 21:35:13 +0300

    MatchResolverin pääluuppi, toistaiseksi tyhjä

--- domainEngine/matches/MatchResolver.ts
+++ domainEngine/matches/MatchResolver.ts
@@ -1,16 +1,23 @@
 import Match from "../../domainCore/Match";
 import MatchEvent from "../../domainCore/MatchEvent";
+import Tactics from "../../domainCore/Tactics";
 import { getRandomNumberInRange } from "../../domainCore/domainUtils";
-import { GAME_DUMMY_MODE } from "../../domainCore/domainProperties";
+import { GAME_DUMMY_MODE, MATCH_GRANULARITY_MINUTES } from "../../domainCore/domainProperties";
 
 // Tarvitaan jonkinlainen MatchSetup-domainolio, ne tänne parametreina
+// lisäksi palautusarvo, jossa eventtien lisäksi balancet ja intensityt
 export const resolveMatch = (): MatchEvent[] => {
     const events: MatchEvent[] = [];
-    events.push(...(GAME_DUMMY_MODE ? playDummy() : play()));
+    events.push(...(GAME_DUMMY_MODE ? playDummy() : play(new Tactics(), new Tactics())));
     return events;
 }
 
-const play = (): MatchEvent[] => {
+// kannasta luetut Tacticsit muuttuvat resolverin työmuistiksi (vaihdot, loukkaantumiset jne) jota ei kirjoiteta takaisin kantaan
+const play = (homeTactics: Tactics, awayTactics: Tactics): MatchEvent[] => {
+    for (let minute = 0; minute < 90; minute += MATCH_GRANULARITY_MINUTES) {
+        // pääluuppi
+    }
+
     return [];
 }
 
